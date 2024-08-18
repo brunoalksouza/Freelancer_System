2448db39125086c21c250c8318628d5b23cff70f
Paulo Silva <pauloecomercial367@gmail.com>
2024-08-18 13:21:35 -0300

    :sparkles: Client handle proposal logic

--- FreelanceSystem/Core/Domain/Entities/Client.cs
+++ FreelanceSystem/Core/Domain/Entities/Client.cs
@@ -1,4 +1,5 @@
 using Domain.Enums;
+using Domain.Exceptions;
 
 namespace Domain.Entities;
 
@@ -11,7 +12,13 @@ public class Client : User
 
     public async Task HandleProposalAsync(Proposal proposal, ProposalAction action)
     {
-        throw new NotImplementedException();
+        if (!proposal.Type.Equals(ProposalType.PROFESSIONAL_PROPOSAL))
+            throw new InvalidOperationException("You only can handle professional proposal types");
+
+        if (proposal.ProfessionalId.Equals(Id))
+            throw new InvalidProposalException("You can't handle a proposal that you send to someone");
+
+        await proposal.SetProposalStatusAsync(action);
     }
 
     public async Task SendProposalToProfessional(Professional professional, Proposal proposal)
