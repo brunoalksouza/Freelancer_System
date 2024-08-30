0e018a08e31f7a1ea46223bd86f408ef0711a7ff
Paulo Silva <pauloecomercial367@gmail.com>
2024-08-29 21:02:17 -0300

    :sparkles: Birth date, cpf props and props validations

--- FreelanceSystem/Core/Domain/Entities/User.cs
+++ FreelanceSystem/Core/Domain/Entities/User.cs
@@ -7,10 +7,12 @@ public abstract class User
 {
     public Guid Id { get; set; }
     public String Name { get; set; }
+    public String CPF { get; set; }
     public String Phone { get; set; }
     public String Email { get; set; }
     public String PasswordHash { get; set; }
     public String ProfilePicture { get; set; }
+    public DateTime BirthDate { get; set; }
     public List<Review> Reviews { get; set; }
     public List<Proposal> Proposals { get; set; }
     public bool ProfileIsCompleted { get; set; } = false;
@@ -33,12 +35,12 @@ public abstract class User
     {
         VerifyProfileIsCompleteAsync();
         var userAlreadyProposal = false;
-        
+
         if (proposal.Type.Equals(ProposalType.PROFESSIONAL_PROPOSAL))
         {
             if (Proposals.Any(p => p.ProfessionalId.Equals(Id)))
                 throw new InvalidProposalException("You can't send a proposal to yourself");
-            
+
             userAlreadyProposal = Proposals.Any(p =>
                 p.ProfessionalId.Equals(proposal.ProfessionalId) &&
                 p.ServiceId.Equals(proposal.ServiceId)
@@ -67,5 +69,59 @@ public abstract class User
         if (ProfileIsCompleted == false)
             throw new ProfileIncompleteException();
     }
-    
+
+    public bool ValidCPF()
+    {
+        if (CPF == null) return false;
+
+        CPF = new string(CPF.Where(char.IsDigit).ToArray());
+
+        if (CPF.Length != 11) return false;
+
+        if (CPF.Distinct().Count() == 1) return false;
+
+        int[] mult1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] mult2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string theresCPF = CPF.Substring(0, 9);
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++)
+            sum += int.Parse(theresCPF[i].ToString()) * mult1[i];
+
+        int resto = sum % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        string digito = resto.ToString();
+        theresCPF = theresCPF + digito;
+        sum = 0;
+
+        for (int i = 0; i < 10; i++)
+            sum += int.Parse(theresCPF[i].ToString()) * mult2[i];
+
+        resto = sum % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        digito = digito + resto.ToString();
+
+        return CPF.EndsWith(digito);
+    }
+
+    public bool IsEighteenYearsOld()
+    {
+        var now = DateTime.Now;
+        var age = now.Year - BirthDate.Year;
+        if (age < 18)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
